875539e3c2972e026eaea9d0a3d6086dda13f32b
krniaaureliyntii <kirania080909@gmail.com>
2026-04-24 09:22:33 +0700

    upload github pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "1": {
+        judul: "Portofolio Pribadi ",
+        keahlian: ["JavaScript","PHP"],
+        gambar: "https://upload.wikimedia.org/wikipedia/commons/2/2f/ABC-2021-LOGO.svg",
+        latar: "ini adalah project apa aja",
+        tantangan: "",
+        pelajaran: "",
+        link: ""
+    },
+};
