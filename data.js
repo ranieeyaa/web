6264a58c852f2cdf8bbe41e7d3a9b47221179c8e
krniaaureliyntii <kirania080909@gmail.com>
2026-04-24 11:51:40 +0700

    Modify project descriptions in data.js
    Updated the project descriptions in the database.

--- data.js
+++ data.js
@@ -3,7 +3,7 @@ const databaseProyek = {
         judul: "Portofolio Pribadi ",
         keahlian: ["JavaScript","PHP"],
         gambar: "https://upload.wikimedia.org/wikipedia/commons/2/2f/ABC-2021-LOGO.svg",
-        latar: "ini adalah project apa aja",
+        latar: "this is my project",
         tantangan: "",
         pelajaran: "",
         link: ""
@@ -13,7 +13,7 @@ const databaseProyek = {
         judul: "Portofolio Pribadi ",
         keahlian: ["JavaScript","PHP"],
         gambar: "https://upload.wikimedia.org/wikipedia/commons/2/2f/ABC-2021-LOGO.svg",
-        latar: "ini adalah project apa aja",
+        latar: "this is my project",
         tantangan: "",
         pelajaran: "",
         link: ""
